cd7edaa5c93b56205aac93ea74f3fc30e67d555f
asbarros <anderson.barros0298@gmail.com>
2019-10-31 11:14:54 -0300

    componente button #02

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -5,27 +5,45 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 
 export default class Calculator extends Component {
+
+    constructor(props) {
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+    }
+
+    clearMemory() {
+        console.log('limpar')
+    }
+
+    setOperation(operation) {
+        console.log(operation, 3)
+    }
+
+    addDigit(n) {
+        console.log(n)
+    }
+
     render() {
         return (
             <div className="calculator" >
                 <Display value={100} />
-                <Button label="AC"/>
-                <Button label="/"/>
-                <Button label="7"/>
-                <Button label="8"/>
-                <Button label="9"/>
-                <Button label="*"/>
-                <Button label="4"/>
-                <Button label="5"/>
-                <Button label="6"/>
-                <Button label="-"/>
-                <Button label="1"/>
-                <Button label="2"/>
-                <Button label="3"/>
-                <Button label="+"/>
-                <Button label="0"/>
-                <Button label="."/>
-                <Button label="="/>
+                <Button label="AC" click={this.clearMemory} triple />
+                <Button label="/" click={() => this.setOperation('/')} operation/>
+                <Button label="7" click={() => this.addDigit('7')} />
+                <Button label="8" click={() => this.addDigit('8')} />
+                <Button label="9" click={() => this.addDigit('9')} />
+                <Button label="*" click={() => this.setOperation('*')} operation/>
+                <Button label="4" click={() => this.addDigit('4')} />
+                <Button label="5" click={() => this.addDigit('5')} />
+                <Button label="6" click={() => this.addDigit('6')} />
+                <Button label="-" click={() => this.setOperation('-')} operation/>
+                <Button label="1" click={() => this.addDigit('1')} />
+                <Button label="2" click={() => this.addDigit('2')} />
+                <Button label="3" click={() => this.addDigit('3')} />
+                <Button label="+" click={() => this.setOperation('+')} operation/>
+                <Button label="0" click={() => this.addDigit('0')} double />
+                <Button label="." click={() => this.addDigit('.')} />
+                <Button label="=" click={() => this.setOperation('=')} operation/>
             </div>
         )
     }
